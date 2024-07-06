45aa59bb6a8a25885a7051a60016556e139f44bd
TARIK TALHA KÜÇÜK <106864990+tariktalha@users.noreply.github.com>
2024-07-06 20:35:20 +0300

    Create turkish_words.py

---
+++ turkish_words.py
@@ -0,0 +1,15 @@
+# turkish_words.py
+
+turkish_words = [
+"oniki",
+ "yirmi"
+  "ince",
+   "zayıf",
+    "yakışıklı",
+     "zeki",
+      "bıyık",
+       "sakal",
+        "sarışın",
+         "düzsaç"
+    # Diğer Türkçe kelimeler buraya eklenebilir
+]
